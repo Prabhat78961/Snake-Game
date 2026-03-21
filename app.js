81a45f395dadb708bb434a30e332f6bf6f900e1e
Prabhat <Prabhat78961@gmail.com>
2026-03-21 10:11:15 +0530

    Fix

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 const blocks = []
+const snake = [{
+    x: 1, y: 3
+}, {
+    x: 1, y: 4
+}, {
+    x: 1, y: 5
+}]
 
 for (let row = 0; row<rows; row++) {
     for (let col = 0; col<cols; col++) {
